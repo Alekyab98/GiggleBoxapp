35d95358b9f9fc042434562de230fd06c7e4ac2b
manojnuvvala <manoj.nuvvala@gmail.com>
2021-11-24 17:35:04 -0600

    made changes in registration and write post

--- app/src/main/java/com/example/smilemaker/MainActivity.java
+++ app/src/main/java/com/example/smilemaker/MainActivity.java
@@ -5,11 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         userName = findViewById(R.id.editTextTextPersonName);
         password = findViewById(R.id.editTextTextPersonName2);

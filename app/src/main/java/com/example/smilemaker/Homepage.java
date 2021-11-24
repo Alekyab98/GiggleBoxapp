d3e4a2e117b472dec2f4eb52fcfd1b22b5b6a1de
Alekyab98 <77812320+Alekyab98@users.noreply.github.com>
2021-11-24 17:58:44 -0600

    made changes in homepage and wall activity also added splash activity

---
+++ app/src/main/java/com/example/smilemaker/Homepage.java
@@ -0,0 +1,44 @@
+package com.example.smilemaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.smilemaker.modal.Jokes;
+
+public class SingleJoke extends AppCompatActivity {
+    String joke;
+    Button btnNextJoke;
+    TextView txtJoke;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        btnNextJoke = findViewById(R.id.btnNextJoke);
+        txtJoke = findViewById(R.id.txtJoke);
+        txtJoke.setText(Jokes.getRandomJoke());
+        btnNextJoke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SingleJoke.this, WallActivity.class);
+                //    i.putExtra("joke", id);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/smilemaker/Splash.java
@@ -0,0 +1,61 @@
+package com.example.smilemaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.smilemaker.modal.Utils;
+
+public class Splash extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
+//        setContentView(R.layout.activity_main);
+        //this will bind your MainActivity.class file with activity_main.
+
+        new Handler().postDelayed(new Runnable() {
+            private SharedPreferences Loginprefs;
+
+            @Override
+            public void run() {
+
+                Loginprefs = getApplicationContext().getSharedPreferences(Utils.PREF_NAME, 0);
+                String userLoginStatus = Loginprefs.getString("userLoginStatus", "not found");
+                if (userLoginStatus.equals("yes")) {
+                    //the user is login
+                    Intent i = new Intent(Splash.this, SingleJoke.class);
+                    //    i.putExtra("joke", id);
+
+                    startActivity(i);
+
+                } else {
+                    Intent i = new Intent(Splash.this,
+                            loginActivity.class);
+                    //Intent is used to switch from one activity to another.
+
+                    startActivity(i);
+                    //invoke the SecondActivity.
+
+                    finish();
+                    //the current activity will get finished.
+                    //user is logout
+                }
+
+
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}

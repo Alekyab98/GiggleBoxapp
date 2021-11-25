7724e48297e79abb9e7733eb357651d06ea81ece
Satyachowdary97 <77590874+Satyachowdary97@users.noreply.github.com>
2021-11-25 14:59:11 -0600

    Added 3 files related to facebook feedback model

---
+++ app/src/main/java/com/example/smilemaker/FacebookFeedModal.java
@@ -0,0 +1,121 @@
+package com.mileset.SmileMaker;
+
+import java.util.Random;
+
+public class FacebookFeedModal {
+    // variables for storing our author image,
+    // author name, postDate,postDescription,
+    // post image,post likes,post comments.
+    private String pId;
+    private String uName;
+    private String authorName;
+    private String postDate;
+    private String postDescription;
+    private String postIV;
+    private String postLikes;
+    private String postComments;
+
+    public String getpId() {
+        return pId;
+    }
+
+    public void setpId(String pId) {
+        this.pId = pId;
+    }
+
+    // creating getter and setter methods.
+    public String getuName() {
+        return uName;
+    }
+
+    public void setuName(String authorImage) {
+        this.uName = authorImage;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(String postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getPostDescription() {
+        return postDescription;
+    }
+
+    public void setPostDescription(String postDescription) {
+        this.postDescription = postDescription;
+    }
+
+    public String getPostIV() {
+        return postIV;
+    }
+
+    public void setPostIV(String postIV) {
+        this.postIV = postIV;
+    }
+
+    public String getPostLikes() {
+        return postLikes;
+    }
+
+    public void setPostLikes(String postLikes) {
+        this.postLikes = postLikes;
+    }
+
+    public String getPostComments() {
+        return postComments;
+    }
+
+    public void setPostComments(String postComments) {
+        this.postComments = postComments;
+    }
+
+    // creating a constructor class.
+    public FacebookFeedModal(String pid, String authorImage, String authorName, String postDate,
+                             String postDescription, String postIV, String postLikes,
+                             String postComments) {
+        this.pId = pid;
+        this.uName = authorImage;
+        this.authorName = authorName;
+        this.postDate = postDate;
+        this.postDescription = postDescription;
+        this.postIV = postIV;
+        this.postLikes = postLikes;
+        this.postComments = postComments;
+    }
+
+    public FacebookFeedModal(String pid, String uName, String FName, String postDate,
+                             String postDescription, String postLikes,
+                             String postComments) {
+        this.pId = pid;
+        this.uName = uName;
+        this.authorName = FName;
+        this.postDate = postDate;
+        this.postDescription = postDescription;
+        this.postLikes = postLikes;
+        this.postComments = postComments;
+    }
+
+    public FacebookFeedModal(String postDescription){
+        this.pId = new Random().nextInt()+"";
+        this.uName = "Anonymous";
+        this.authorName = "Anonymous";
+        this.postDate = "";
+        this.postDescription = postDescription;
+
+        this.postComments = "";
+
+
+    }
+}
+
---
+++ app/src/main/java/com/example/smilemaker/FacebookFeedRVAdapter.java
@@ -0,0 +1,205 @@
+package com.mileset.SmileMaker;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mileset.SmileMaker.controller.CommentHandler;
+import com.mileset.SmileMaker.controller.PostHandler;
+import com.mileset.SmileMaker.modal.Utils;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class FacebookFeedRVAdapter extends RecyclerView.Adapter<FacebookFeedRVAdapter.ViewHolder> {
+
+    // arraylist for our facebook feeds.
+    private ArrayList<FacebookFeedModal> facebookFeedModalArrayList;
+    private Context context;
+
+    // creating a constructor for our adapter class.
+    public FacebookFeedRVAdapter(ArrayList<FacebookFeedModal> facebookFeedModalArrayList, Context context) {
+        this.facebookFeedModalArrayList = facebookFeedModalArrayList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public FacebookFeedRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // inflating our layout for item of recycler view item.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.facebook_feed_rv_item, parent, false);
+        return new FacebookFeedRVAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FacebookFeedRVAdapter.ViewHolder holder, int position) {
+        // getting data from array list and setting it to our modal class.
+        FacebookFeedModal modal = facebookFeedModalArrayList.get(position);
+
+        // setting data to each view of recyclerview item.
+        //  Picasso.get().load(modal.getAuthorImage()).into(holder.authorIV);
+
+        holder.postID.setText(modal.getpId());
+        holder.authorNameTV.setText(modal.getAuthorName());
+        holder.timeTV.setText(modal.getPostDate());
+        holder.descTV.setText(modal.getPostDescription());
+        //Picasso.get().load(modal.getPostIV()).into(holder.postIV);
+        //  holder.likesTV.setText(modal.getPostLikes());
+   /*     SharedPreferences Loginprefs = context.getSharedPreferences(Utils.PREF_NAME, 0);
+        String name = Loginprefs.getString("name", "not found");
+        System.out.println(modal.getAuthorName() + "----" + name);
+        if (!modal.getAuthorName().equals(name)) {
+            System.out.println("true");
+
+
+            holder.delButton.setVisibility(View.GONE);
+        }
+*/
+        CommentHandler db11 = new CommentHandler(context);
+        int i = db11.countComments(modal.getpId());
+        System.out.println("count: " + i);
+        holder.commentsTV.setText(Integer.toString(i));
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        // returning the size of our array list.
+        return facebookFeedModalArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // creating variables for our views
+        // of recycler view items.
+        ImageView imgComment;
+        ImageButton delButton, editButton;
+        private CircleImageView authorIV;
+        private TextView authorNameTV, timeTV, descTV, postID;
+        //private ImageView postIV;
+        private TextView commentsTV;
+        //    TextView likesTV;
+        //private LinearLayout shareLL;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // initializing our variables
+            //  shareLL = itemView.findViewById(R.id.idLLShare);
+            delButton = itemView.findViewById(R.id.delButton);
+            editButton = itemView.findViewById(R.id.editButton);
+            authorIV = itemView.findViewById(R.id.idCVAuthor);
+            authorNameTV = itemView.findViewById(R.id.idTVAuthorName);
+            timeTV = itemView.findViewById(R.id.idTVTime);
+            descTV = itemView.findViewById(R.id.idTVDescription);
+            imgComment = itemView.findViewById(R.id.imgComment);
+            postID = itemView.findViewById(R.id.postID);
+            //  postIV = itemView.findViewById(R.id.idIVPost);
+            //likesTV = itemView.findViewById(R.id.idTVLikes);
+            commentsTV = itemView.findViewById(R.id.idTVComments);
+
+
+            //         commentsTV.setText(i);
+//Toast.makeText(context,list.size(),Toast.LENGTH_SHORT).show();
+
+            imgComment.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(context.getApplicationContext(), postID.getText().toString().trim(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context.getApplicationContext(), CommentsActivity.class);
+                    i.putExtra("commentID", postID.getText().toString().trim());
+                    context.startActivity(i);
+                }
+            });
+
+
+            editButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(context.getApplicationContext(),
+                            EditActivity.class);
+
+                    i.putExtra("jokeId", postID.getText().toString().trim());
+                    i.putExtra("joke", descTV.getText().toString().trim());
+
+                    context.startActivity(i);
+
+                }
+            });
+
+
+            delButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    PostHandler db1 = new PostHandler(context.getApplicationContext());
+                    CommentHandler db2 = new CommentHandler(context.getApplicationContext());
+                    new AlertDialog.Builder(context)
+                            .setTitle("Delete Post")
+                            .setMessage("Are you sure??")
+                            .setPositiveButton("Yes", (dialog, which) -> {
+                                //doSomething();
+
+                                SharedPreferences Loginprefs = context.getSharedPreferences(Utils.PREF_NAME, 0);
+                                String name = Loginprefs.getString("name", "not found");
+                                //System.out.println(modal.getAuthorName() + "----" + name);
+                                if (!authorNameTV.getText().toString().trim().equals(name)) {
+                                    System.out.println("true");
+                                    Toast.makeText(context.getApplicationContext(), "You are not allowed to delete this post", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+
+
+                                String postId = postID.getText().toString().trim();
+
+                                db2.delete(postId);
+                                db1.delete(postId);
+
+                                Intent i = new Intent(context.getApplicationContext(),
+                                        WallActivity.class);
+                                //Intent is used to switch from one activity to another.
+
+                                context.startActivity(i);
+                                //invoke the SecondActivity.
+
+
+                                dialog.dismiss();
+                            })
+                            .setNegativeButton("No", (dialog, which) -> dialog.dismiss())
+                            .show();
+
+                    //      Toast.makeText(context.getApplicationContext(), postID.getText().toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+            commentsTV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(context.getApplicationContext(), postID.getText().toString().trim(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context.getApplicationContext(), CommentsActivity.class);
+                    i.putExtra("postID", postID.getText().toString().trim());
+                    context.startActivity(i);
+                }
+            });
+        }
+    }
+}
+
